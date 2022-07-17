78661a219e0adee5fc2717a530933376f54f2cab
rcabret <ricardo.cabret@gmail.com>
2022-07-17 00:17:24 -0400

    stays

--- src/_components/PropertyGridItem/index.tsx
+++ src/_components/PropertyGridItem/index.tsx
@@ -2,23 +2,34 @@ import React from 'react'
 import { ItemWrapper, Metadata } from './styles'
 import GridImage from '../UI/GridImage'
 import Header from '../Typography/Header'
+import BodyText from '../Typography/BodyText'
 
 interface GridItemProps {
     propertyObj: any
 }
 const PropertyGridItem = ({ propertyObj }: GridItemProps) => {
-    console.log(propertyObj)
+    const { bannerDescriptionList, tileImage, propertyName } = propertyObj
+
     return (
         <ItemWrapper>
             <GridImage
                 borderRadius={false}
                 border={false}
                 ratio={0.68}
-                imageObj={propertyObj.tileImage}
+                imageObj={tileImage}
             />
             <Metadata>
+                <BodyText size="sm">
+                    {Array.isArray(bannerDescriptionList)
+                        ? bannerDescriptionList.map((x, i) => {
+                              return `${x} ${
+                                  i < bannerDescriptionList.length - 1 ? '· ' : ''
+                              }`
+                          })
+                        : bannerDescriptionList}
+                </BodyText>
                 <Header bold={false} size={2}>
-                    {propertyObj.propertyName}
+                    {propertyName}
                 </Header>
             </Metadata>
         </ItemWrapper>
--- src/_components/PropertyGridItem/styles.ts
+++ src/_components/PropertyGridItem/styles.ts
@@ -11,8 +11,14 @@ export const ItemWrapper = styled.div`
 export const Metadata = styled.aside`
     padding: ${rem('10px')} ${rem('14px')} ${rem('14px')} ${rem('14px')};
 
+    p {
+        opacity: 0.4;
+    }
+
     h2 {
+        margin-top: ${rem('10px')};
         font-size: ${rem('32px')};
         font-weight: 200;
+        width: 100%;
     }
 `
