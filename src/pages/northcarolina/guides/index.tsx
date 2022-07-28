ac764e42c5ad16c3e86af4c16dc1b6d0b85a9869
rcabret <ricardo.cabret@gmail.com>
2022-07-28 10:46:24 -0400

    stays

---
+++ src/pages/northcarolina/guides/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const NCGuides = () => (
+    <div>North Carolina Guides</div>
+)
+
+export default NCGuides;
---
+++ src/pages/puertorico/guides/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const PRGuides = () => (
+    <div>Puerto Rico Guides</div>
+)
+
+export default PRGuides;
