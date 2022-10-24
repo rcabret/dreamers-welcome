01abc8674398c26e430dd031d66420d25c79a71b
ricardo.cabret <ricardo.cabret@vimeo.com>
2022-10-24 19:01:01 -0400

    breakpoints

--- src/_components/Suite/Highlight/styles.ts
+++ src/_components/Suite/Highlight/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { rem } from 'polished'
+import { BREAKPOINTS } from '../../../_constants/brekpoints'
 
 export const HighlightBlurb = styled.div`
     width: 100%;
@@ -8,6 +9,13 @@ export const HighlightBlurb = styled.div`
     > p {
         margin-left: calc(25% + 20px);
         max-width: ${rem('570px')};
+    }
+
+    @media (max-width: ${BREAKPOINTS.TABLET}) {
+        > p {
+            margin-left: 0;
+            padding: 0 ${rem(20)};
+        }
     }
 `
 
@@ -26,5 +34,10 @@ export const SliderWrap = styled.div`
     .swiper-slide {
         width: auto;
     }
-  
+
+    @media (max-width: ${BREAKPOINTS.TABLET}) {
+        .swiper {
+            margin-left: ${rem(20)};
+        }
+    }
 `
