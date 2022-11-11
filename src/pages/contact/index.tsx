8932e6d2fd4839e73b266fb8415f7c3627633b64
ricardo.cabret <ricardo.cabret@vimeo.com>
2022-11-11 12:48:54 -0500

    Contact backend done

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,27 +1,101 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import { Content } from '../../styles/global'
+import styled from 'styled-components'
+import { getAllProperties } from '../../_lib/api'
+import { rem } from 'polished'
+import Header from '../../_components/Typography/Header'
 
-const Contact = ({ setNavTheme }: any) => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [subject, setSubject] = useState('')
-    const [property, setProperty] = useState('')
-    const [message, setMessage] = useState('')
-    const [submitted, setSubmitted] = useState(false)
+const FormContainer = styled.div`
+    max-width: ${rem(700)};
+    margin: ${rem(40)} auto;
+    padding: ${rem(40)};
+    border-radius: ${rem(10)};
+    border: 1px solid #c1c1c1;
+    margin-bottom: ${rem(100)};
 
+    input,
+    select,
+    textarea {
+        width: 100%;
+        outline: none;
+        box-shadow: none;
+        border: 1px solid #c1c1c1;
+        font-size: ${rem(18)} !important;
+        display: block;
+        margin-bottom: ${rem(20)};
+        font-family: 'Yellix', sans-serif !important;
+        padding: ${rem(10)} ${rem(20)};
+        background: rgba(217, 217, 217, 0.45);
+    }
+
+    textarea {
+        padding: ${rem(20)};
+        resize: none;
+    }
+
+    input[type='submit'] {
+        background: black;
+        border-radius: ${rem(40)};
+        color: white;
+    }
+
+    h4,
+    h2 {
+        text-align: center;
+        margin-bottom: ${rem(40)};
+        display: inline-block;
+        width: 100%;
+    }
+
+    h2 {
+        margin-top: ${rem(40)};
+        padding-bottom: ${rem(30)};
+        border-bottom: 1px solid #c1c1c1;
+        margin-bottom: ${rem(60)};
+    }
+`
+
+const StyledInput = styled.input`
+    border-radius: ${rem(40)};
+    height: ${rem(50)};
+    padding: ${rem(20)};
+`
+
+const StyledSelect = styled.select`
+    border-radius: ${rem(40)};
+    height: ${rem(50)};
+
+    option:disabled {
+        color: #c1c1c1 !important;
+    }
+
+    label {
+        position: relative;
+    }
+    label:after {
+        right: 80px;
+    }
+`
+
+const StyledTextarea = styled.textarea`
+    border-radius: ${rem(10)};
+    height: ${rem(150)};
+`
+
+const Contact = ({ properties, setNavTheme }: any) => {
     useEffect(() => {
         setNavTheme('dark')
     }, [])
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-        let data = {
-            name,
-            subject,
-            property,
-            email,
-            message,
-        }
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const _handleSubmit = (data: any) => {
+        const { name, subject, property, email, message } = data
 
         fetch('/api/contact', {
             method: 'POST',
@@ -35,9 +109,56 @@ const Contact = ({ setNavTheme }: any) => {
 
     return (
         <Content padding>
-            <div onClick={(e) => handleSubmit(e)}>Hello here goes a form</div>
+            <FormContainer>
+                <Header size={4}>CONTACT</Header>
+                <Header size={2}>Get in touch with us!</Header>
+                <form onSubmit={handleSubmit((data) => console.log(data))}>
+                    <StyledInput
+                        {...register('Name')}
+                        placeholder="Full Name"
+                    />
+                    <StyledInput
+                        placeholder="Email"
+                        type="email"
+                        {...register('email', { required: true })}
+                    />
+                    {errors.email && <p>Please enter an email format.</p>}
+                    <StyledInput
+                        {...register('subject')}
+                        placeholder="Subject"
+                    />
+                    {errors.age && <p>Please enter number for age.</p>}
+                    <StyledSelect {...register('property')}>
+                        <>
+                            <option value="" disabled selected>
+                                Select property
+                            </option>
+                            {properties &&
+                                properties.length &&
+                                properties.map((p: any) => (
+                                    <option>{p.propertyName}</option>
+                                ))}
+                        </>
+                    </StyledSelect>
+                    <StyledTextarea
+                        {...register('message')}
+                        placeholder="Message"
+                    />
+                    <StyledInput type="submit" value="Message Us" />
+                </form>
+            </FormContainer>
         </Content>
     )
 }
 
 export default Contact
+
+export async function getStaticProps() {
+    const properties = await getAllProperties()
+
+    return {
+        props: {
+            properties,
+        },
+    }
+}
