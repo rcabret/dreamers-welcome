55e721865f20e016999ff61b6e1b1dce0fc7efe0
ricardo.cabret <ricardo.cabret@vimeo.com>
2022-11-12 13:50:14 -0500

    Hello

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -42,68 +42,75 @@ const Contact = ({ properties, setNavTheme }: any) => {
 
     return (
         <Content padding>
-            {!submitted ? (
-                <FormContainer>
-                    <Header size={4}>CONTACT</Header>
-                    <Header size={2}>Get in touch with us!</Header>
-                    <form
-                        onSubmit={handleSubmit((data) => _handleSubmit(data))}
-                    >
-                        <StyledInput
-                            {...register('name', { required: true })}
-                            placeholder="NAME"
-                        />
-                        {errors.name && <p>Please enter your name.</p>}
-                        <StyledInput
-                            placeholder="EMAIL"
-                            type="email"
-                            {...register('email', { required: true })}
-                        />
-                        {errors.email && <p>Please enter an email format.</p>}
-                        <StyledInput
-                            {...register('subject', { required: true })}
-                            placeholder="SUBJECT"
-                        />
-                        {errors.subject && <p>Please select subject.</p>}
-                        <StyledSelectWrapper>
-                            <StyledSelect
-                                {...register('property', {
-                                    required: true,
-                                })}
-                            >
-                                <>
-                                    <option value="" disabled selected>
-                                        SELECT PROPERTY
-                                    </option>
-                                    {properties &&
-                                        properties.length &&
-                                        properties.map((p: any) => (
-                                            <option>
-                                                {p.propertyName.toUpperCase()}
-                                            </option>
-                                        ))}
-                                </>
-                            </StyledSelect>
-                            <Chevron dark />
-                        </StyledSelectWrapper>
-                        {errors.property && <p>Please select subject.</p>}
-                        <StyledTextarea
-                            {...register('message', { required: true })}
-                            placeholder="MESSAGE"
-                        />
-                        {errors.message && <p>Please enter message.</p>}
-                        <StyledInput
-                            type="submit"
-                            value="MESSAGE US"
-                            disabled={Object.keys(errors).length}
-                        />
-                    </form>
-                </FormContainer>
-            ) : (
-                <StyledSuccessBodyText size="xlg">
-                    Thank you for contacting us! We will be in touch shortly.
-                </StyledSuccessBodyText>
-            )}
+            <div style={{ margin: '0 15px' }}>
+                {!submitted ? (
+                    <FormContainer>
+                        <Header size={4}>CONTACT</Header>
+                        <Header size={2}>Get in touch with us!</Header>
+                        <form
+                            onSubmit={handleSubmit((data) =>
+                                _handleSubmit(data)
+                            )}
+                        >
+                            <StyledInput
+                                {...register('name', { required: true })}
+                                placeholder="NAME"
+                            />
+                            {errors.name && <p>Please enter your name.</p>}
+                            <StyledInput
+                                placeholder="EMAIL"
+                                type="email"
+                                {...register('email', { required: true })}
+                            />
+                            {errors.email && (
+                                <p>Please enter an email format.</p>
+                            )}
+                            <StyledInput
+                                {...register('subject', { required: true })}
+                                placeholder="SUBJECT"
+                            />
+                            {errors.subject && <p>Please select subject.</p>}
+                            <StyledSelectWrapper>
+                                <StyledSelect
+                                    {...register('property', {
+                                        required: true,
+                                    })}
+                                >
+                                    <>
+                                        <option value="" disabled selected>
+                                            SELECT PROPERTY
+                                        </option>
+                                        {properties &&
+                                            properties.length &&
+                                            properties.map((p: any) => (
+                                                <option>
+                                                    {p.propertyName.toUpperCase()}
+                                                </option>
+                                            ))}
+                                    </>
+                                </StyledSelect>
+                                <Chevron dark />
+                            </StyledSelectWrapper>
+                            {errors.property && <p>Please select subject.</p>}
+                            <StyledTextarea
+                                {...register('message', { required: true })}
+                                placeholder="MESSAGE"
+                            />
+                            {errors.message && <p>Please enter message.</p>}
+                            <StyledInput
+                                type="submit"
+                                value="MESSAGE US"
+                                disabled={Object.keys(errors).length}
+                            />
+                        </form>
+                    </FormContainer>
+                ) : (
+                    <StyledSuccessBodyText size="xlg">
+                        Thank you for contacting us! We will be in touch
+                        shortly.
+                    </StyledSuccessBodyText>
+                )}
+            </div>
         </Content>
     )
 }
