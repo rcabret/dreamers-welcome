524ef9f94131089d892f6889682869a36c22c200
ricardo.cabret <ricardo.cabret@vimeo.com>
2022-11-16 08:48:11 -0500

    show reviews

--- src/_components/ReviewItem/styles.ts
+++ src/_components/ReviewItem/styles.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
 import { ItemWrapper } from '../../styles/global'
 import { rem } from 'polished'
+import { BREAKPOINTS } from '../../_constants/brekpoints'
 
 export const ItemWrapperStyled = styled(ItemWrapper)`
     padding: ${rem(10)};
     border-radius: ${rem(20)};
     box-shadow: 0 20px 50px rgba(18, 17, 39, 0.08);
+
+    @media (max-width: ${BREAKPOINTS.TABLET}) {
+        box-shadow: none;
+        border: 1px solid #c1c1c1;
+        padding: ${rem(20)};
+    }
 `
 
 export const TopSection = styled.div`
