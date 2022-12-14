c6d5948c35983619bfb27c3e396ad8191bf8389a
ricardo.cabret <ricardo.cabret@vimeo.com>
2022-12-14 18:30:25 -0500

    New contact for with sendGrid

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,57 +1,10 @@
 export default async function (req: any, res: any) {
-    let postmark = require('postmark')
-    const serverToken = '74c559ed-0f3c-41d4-875d-491df406d88d'
-    const client = new postmark.ServerClient(serverToken)
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
     const mailData = {
-        From: 'stay@dreamerspuertorico.com',
-        To: 'ricardo.cabret@gmail.com',
-        Subject: `[${req.body.bucket}] | [${req.body.subject}] New Form Submission from ${req.body.name}`,
-        TextBody: req.body.message + ' | Sent from: ' + req.body.email,
-        HtmlBody: `<p>Sent from: ${req.body.email}</p>
-                <div><strong>Name:</strong> ${req.body.name}</div>
-                <div><strong>Subject:</strong> ${req.body.subject}</div>
-                <div><strong>Property:</strong> ${req.body.property}</div>
-                <div><strong>Destination:</strong> ${req.body.bucket}</div>
-                <div><strong>Message:</strong></div>
-                <div><p>${req.body.message}</p></div>`,
-    }
-
-    await client.sendEmail(mailData, (error: any, result: any) => {
-        if (error) {
-        } else {
-            if (result.Message === 'OK') {
-                res.status(200)
-                res.end()
-            }
-        }
-    })
-
-    /*let nodemailer = require('nodemailer')
-
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: 'smtp.gmail.com',
-        auth: {
-            user: 'dw.contact.form@gmail.com',
-            pass: process.env.NEXT_PUBLIC_BURNER_EMAIL_PW,
-        },
-        secure: true,
-    })
-
-    const getCorrectEmail = (bucket: string) => {
-        switch (bucket) {
-            case 'NORTH CAROLINA':
-                return 'info@dreamerswelcome.com'
-            default:
-            case 'PUERTO RICO':
-                return 'stay@dreamerspuertorico.com'
-        }
-    }
-
-    const mailData = {
-        from: 'New DW Contact Form Submission',
-        to: 'stay@dreamerspuertorico.com',
+        to: 'ricardo.cabret@gmail.com',
+        from: 'dreamerswelcome.web@gmail.com',
         subject: `[${req.body.bucket}] | [${req.body.subject}] New Form Submission from ${req.body.name}`,
         text: req.body.message + ' | Sent from: ' + req.body.email,
         html: `<p>Sent from: ${req.body.email}</p>
@@ -63,17 +16,13 @@ export default async function (req: any, res: any) {
                 <div><p>${req.body.message}</p></div>`,
     }
 
-    try {
-        await transporter.sendMail(mailData, function (err: any, info: any) {
-            if (err) {
-                res.error(err)
-                res.end()
-            } else console.log(info)
+    sgMail
+        .send(mailData)
+        .then(() => {
+            res.status(200)
+            res.end()
         })
-    } catch (error: any) {
-        return res
-            .status(500)
-            .json({ error: error.message || error.toString() })
-    }
-    return res.status(200).json({ error: '' })*/
+        .catch((error: any) => {
+            res.status(500).json({ error: error.message || error.toString() })
+        })
 }
