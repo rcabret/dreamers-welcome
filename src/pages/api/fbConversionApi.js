423ae3eb55719cacdef94e7d59cf6533c8d61403
jin-1206 <softdev.1206@gmail.com>
2023-05-31 11:16:08 -0400

    Update fbConversionApi.js

--- src/pages/api/fbConversionApi.js
+++ src/pages/api/fbConversionApi.js
@@ -3,7 +3,7 @@ export async function sendConversionEvent(eventData) {
   const access_token = process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN;
   const pixelID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
-  const apiUrl = `https://graph.facebook.com/v17.0/${pixelID}/events?access_token=${access_token}`;
+  const apiUrl = `https://graph.facebook.com/v16.0/${pixelID}/events?access_token=${access_token}`;
   try {
     const response = await axios.post(apiUrl, {
       data: [eventData],
