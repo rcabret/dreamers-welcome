7c45a64b059759b3ee3da9a3c59795c3bc727e97
shivam-ultivic <shivam.ultivic@gmail.com>
2023-08-16 18:44:21 +0530

    open in new tab

--- src/_components/Footer/index.tsx
+++ src/_components/Footer/index.tsx
@@ -44,7 +44,7 @@ const Footer = ({ activeBucket }: any) => {
                         <Link href={'/faq/general'}>FAQs</Link>
                     </li>
                     <li className='text-sm sm:text-xl'>
-                        <Link href="https://giftup.app/place-order/f4fbef08-de2d-4d8e-c896-08db98c383e8?platform=hosted" target="_blank">GIFT CARD</Link>
+                        <a href="https://giftup.app/place-order/f4fbef08-de2d-4d8e-c896-08db98c383e8?platform=hosted" target="_blank">GIFT CARD</a>
                     </li>
                 </ul>
                 <ul>
