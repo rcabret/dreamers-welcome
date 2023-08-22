7a85f156ed2e047d0bf83900d7abd89183e9cabb
shivam-ultivic <shivam.ultivic@gmail.com>
2023-08-22 16:33:52 +0530

    news-section-bug-fix

--- src/_components/NewsItem/index.tsx
+++ src/_components/NewsItem/index.tsx
@@ -1,147 +1,59 @@
-import React, { useContext, useEffect } from 'react'
-import { Panel, MainList, DropdownWrapper } from './styles'
-import Link from 'next/link'
-import Button from '../../UI/Buttons/Button'
-import { bucketLinks } from '../../../_constants/links'
-import { viewportContext } from '../../../_utils/ViewportProvider'
-import {NavbarDropdown} from "../styles";
-import { useRouter } from 'next/router';
+import React, { useContext, useEffect, useState } from 'react'
+import BodyText from '../Typography/BodyText'
+import Header from '../Typography/Header'
+import GridImage from '../UI/GridImage'
+import moment from 'moment'
+import { News } from '../../_constants/DataTypes'
+import { ItemWrapperStyled, NewsTextWrapper } from './styles'
+import { viewportContext } from '../../_utils/ViewportProvider'
+import { getNews, getNewsEntry } from '../../_lib/api'
 
-const MenuPanel = ({ opened, activeBucket, onClose }: any) => {
-    const router = useRouter();
-    const getLink = (slug: string) =>
-        `/${slug}${
-            activeBucket
-                ? `/${activeBucket.toLowerCase().replace(' ', '')}`
-                : ''
-        }`
-
-    const runMainList = (remove = false) => {
-        const list: HTMLCollection | null =
-            document.querySelector('#main_list').children
-        if (list) {
-            for (let i = 0; i < list.length; i++) {
-                const ele = list[i]
-                setTimeout(
-                    () => {
-                        if (!remove) {
-                            ele.classList.add('active')
-                        } else {
-                            ele.classList.remove('active')
-                        }
-                    },
-                    !remove ? i * 100 : 0
-                )
-            }
-        }
-    }
-
+const NewsItem = ({ newsObj }: { newsObj: News }) => {
+    const { date, title, text, tileImage, slug } = newsObj
+    const [_res, setRes] = useState()
+    const [id, setId] = useState();
+    
+    const stringDate = moment(date).format('MMMM Do YYYY')
     const breakpoint = useContext(viewportContext)
 
     useEffect(() => {
-        if (opened) {
-            runMainList()
-        } else {
-            setTimeout(() => {
-                runMainList(true)
-            }, 500)
+        async function fetchData() {
+          try {
+            const newsData = await getNews();
+            setRes(newsData);
+          } catch (error) {
+            console.error("Error fetching news:", error);
+          }
         }
-    }, [opened])
+      
+        fetchData();
+      }, []);
+    
+    const handleClick = async () => {
+        const filteredArray = _res.filter(item => item.fields.title == title);
+        const sys = filteredArray.map((x: {sys: string }) => x.sys.id)
+        setId(sys)        
+    }
 
     return (
-        <Panel opened={opened}>
-            <div>
-                {breakpoint !== 'desktop' && (
-                    <DropdownWrapper>
-                        <NavbarDropdown
-                            bucket={activeBucket}
-                            dark
-                            id="topnav-mobile-drop"
-                            links={bucketLinks}
-                            defaultLabel="CHOOSE DESTINATION"
-                        />
-                    </DropdownWrapper>
-                )}
-                <MainList id="main_list" onClick={() => onClose(false)}>
-                    <li>
-                        <Link href={router.query ? getLink('stays') : ""}>STAYS</Link>
-                    </li>
-                    <li>
-                        <Link href="/news">NEWS</Link>
-                    </li>
-                    <li>
-                        <Link href={router.query ? getLink('experiences') : ""}>EXPERIENCES</Link>
-                        {!activeBucket && <aside />}
-                    </li>
-                    <li>
-                        <Link href={router.query ? getLink('guidebooks') : ""}>GUIDEBOOKS</Link>
-                        {!activeBucket && <aside />}
-                    </li>
-                </MainList>
-                <div className="anchorSection flex justify-around" onClick={() => onClose(false)}>
-                    <ul>
-                        <li>
-                            <Link href="/about">ABOUT</Link>
-                        </li>
-                        <li>
-                            <Link href="/faq/general">FAQs</Link>
-                        </li>
-                        <li>
-                            <a
-                                href="https://open.spotify.com/user/krlki7u9768cfjkk49xb4iz6n"
-                                target="_blank"
-                            >
-                                DW RADIO
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://giftup.app/place-order/f4fbef08-de2d-4d8e-c896-08db98c383e8?platform=hosted"
-                                target="_blank"
-                            >
-                                GIFT CARDS
-                            </a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <a
-                                href="https://www.instagram.com/dreamers.welcome"
-                                target="_blank"
-                            >
-                                INSTA
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://www.tiktok.com/@dreamers.welcome"
-                                target="_blank"
-                            >
-                                TIKTOK
-                            </a>
-                        </li>
-                        <li>
-                            <a href='https://www.pinterest.com/dreamers_welcome'
-                                target="_blank"
-                            >
-                                PINTEREST
-                            </a>
-                        </li>
-                        <li>
-                            <a href='https://m.facebook.com/dreamers.puertorico'
-                                target="_blank"
-                            >
-                                FACEBOOK
-                            </a>
-                        </li>
-                    </ul>
-                    <Button inverse href="/contact">
-                        CONTACT US
-                    </Button>
+        <a
+          onClick={handleClick}
+          href={`/news/${id}`}
+        >
+            <ItemWrapperStyled>
+                <div className="border">
+                    <Header size={3}>{title}</Header>
+                    {breakpoint !== 'mobile' && (
+                        <NewsTextWrapper>
+                            <BodyText size="sm">{stringDate}</BodyText>
+                            <BodyText size="sm">{text.slice(0, 200)}...</BodyText>
+                        </NewsTextWrapper>
+                    )}
+                    <GridImage border={false} imageObj={tileImage} />
                 </div>
-            </div>
-        </Panel>
+            </ItemWrapperStyled>
+        </a>
     )
 }
 
-export default MenuPanel
+export default NewsItem
