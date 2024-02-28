fbe09b3dd3bdb4f0197cb3f7dd4d8f76d5481424
shivam <shivam.ultivic@gmail.com>
2024-02-28 15:31:16 +0530

    800mb

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
     },
     experimental: {
         esmExternals: false,
-        largePageDataBytes:  300 * 1024 * 1024,
+        largePageDataBytes:  800 * 1024 * 1024,
     }
 }
 
