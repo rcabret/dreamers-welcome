ef9a4e7e98e7f895793568795a2771a9efb4f3e1
shivam <shivam.ultivic@gmail.com>
2024-06-10 12:19:41 +0530

    add new xml file

---
+++ next-sitemap.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://www.dreamerswelcome.com/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  outDir: './public', // Directory where sitemap files will be generated
+  // exclude: ['/secret-page'], // Exclude specific internal pages if needed
+  additionalPaths: async (config) => {
+    const externalUrls = [
+      'https://externalwebsite.com/page1',
+      'https://externalwebsite.com/page2',
+    ];
+
+    return externalUrls.map((url) => ({
+      loc: url,
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }));
+  },
+};
