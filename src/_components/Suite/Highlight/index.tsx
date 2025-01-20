5dd99872260ed19d291ca2da145f525c907676bc
akshita-tomar <akshita.tomar@ultivic.com>
2025-01-20 12:34:48 +0530

    spacing added

--- src/_components/Suite/Highlight/index.tsx
+++ src/_components/Suite/Highlight/index.tsx
@@ -1,75 +1,103 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { HighlightBlurb, SliderWrap } from './styles'
-import BodyText from '../../Typography/BodyText'
-import ImageGridSlider from '../../UI/Swiper'
-import { ContentfulImage } from '../../../_constants/DataTypes'
-import Block from '../../UI/Block'
-import { viewportContext } from '../../../_utils/ViewportProvider'
-import MarkdownModule from '../../Typography/MarkdownModule'
-
+import React, { useContext, useEffect, useState } from 'react';
+import { HighlightBlurb, SliderWrap } from './styles';
+import BodyText from '../../Typography/BodyText';
+import ImageGridSlider from '../../UI/Swiper';
+import { ContentfulImage } from '../../../_constants/DataTypes';
+import Block from '../../UI/Block';
+import { viewportContext } from '../../../_utils/ViewportProvider';
+import MarkdownModule from '../../Typography/MarkdownModule';
 
 interface HighlightProps {
-    title: string
-    blurb: string
-    images: ContentfulImage[]
-    hideSeparator?: boolean
-    slug: string
+  title: string;
+  blurb: string;
+  images: ContentfulImage[];
+  hideSeparator?: boolean;
+  slug: string;
 }
+
 const Highlight = ({
-    title,
-    blurb,
-    images,
-    hideSeparator = false,
-    slug,
+  title,
+  blurb,
+  images,
+  hideSeparator = false,
+  slug,
 }: HighlightProps) => {
-    const breakpoint = useContext(viewportContext)
+  const breakpoint = useContext(viewportContext);
 
-    const getCarouselHeight = () => {
-        switch (breakpoint) {
-            case 'tablet':
-                return 400
-            case 'mobile':
-                return 300
-            case 'desktop':
-            default:
-                return 500
-        }
+  const getCarouselHeight = () => {
+    switch (breakpoint) {
+      case 'tablet':
+        return 400;
+      case 'mobile':
+        return 300;
+      case 'desktop':
+      default:
+        return 500;
     }
+  };
 
-    const [height, setHeight] = useState(getCarouselHeight())
-    
+  const getCenterMode = () => {
+    switch (breakpoint) {
+      case 'tablet':
+        return false;
+      case 'mobile':
+        return true;
+      case 'desktop':
+      default:
+        return false;
+    }
+  };
 
+  const getBreakpoints = () => ({
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 15,
+    },
+  
+  });
 
-    
-    useEffect(() => {
-        setHeight(getCarouselHeight())
-    }, [breakpoint])
+  const [height, setHeight] = useState(getCarouselHeight());
+  const [centerMode, setCenterMode] = useState(getCenterMode());
+  const [breakpoints, setBreakpoints] = useState(getBreakpoints());
 
+  useEffect(() => {
+    setHeight(getCarouselHeight());
+    setCenterMode(getCenterMode());
+    setBreakpoints(getBreakpoints());
+  }, [breakpoint]);
 
-    return (
-        <Block
-            title={title}
-            fullWidth
-            hideSeparator={hideSeparator}
-            showOverflow
-            noPaddingBottom
-            content={
-                <>
-                    <SliderWrap className='slider_wrapper'>
-                        <ImageGridSlider
-                            fixedHeight={height}
-                            slug={slug}
-                            items={images}
-                            spaceBetween={20}
-                        />
-                    </SliderWrap>
-                    <HighlightBlurb>
-                        {blurb && <MarkdownModule data={blurb} />}
-                    </HighlightBlurb>
-                </>
-            }
-        />
-    )
-}
+  return (
+    <Block
+      title={title}
+      fullWidth
+      hideSeparator={hideSeparator}
+      showOverflow
+      noPaddingBottom
+      content={
+        <>
+          <SliderWrap className="slider_wrapper">
+            <ImageGridSlider
+              fixedHeight={height}
+              slug={slug}
+              items={images}
+              spaceBetween={20}
+              centeredSlides={true}
+              slidesOffsetBefore={10} // Add padding on the left
+              slidesOffsetAfter={10}
+              breakpoints={breakpoints}
+            />
+          </SliderWrap>
+          <HighlightBlurb>
+            {blurb && <MarkdownModule data={blurb} />}
+          </HighlightBlurb>
+        </>
+      }
+    />
+  );
+};
 
-export default Highlight
+export default Highlight;
--- src/_components/UI/Swiper/index.tsx
+++ src/_components/UI/Swiper/index.tsx
@@ -1,148 +1,158 @@
 // Import Swiper React components
-import { SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import React, { Component } from 'react'
-import GridImage from '../GridImage'
-import { NavigationWrap, StyledSwiper } from './styles'
-import Swiper from 'swiper'
-import { ContentfulImage } from '../../../_constants/DataTypes'
-import dynamic from 'next/dynamic'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import React, { Component } from 'react';
+import GridImage from '../GridImage';
+import { NavigationWrap, StyledSwiper } from './styles';
+import Swiper from 'swiper';
+import { ContentfulImage } from '../../../_constants/DataTypes';
+import dynamic from 'next/dynamic';
 
-const PropertyGridItem = dynamic(() => import('../../PropertyGridItem'))
+const PropertyGridItem = dynamic(() => import('../../PropertyGridItem'));
 
 interface SwiperProps {
-    items: ContentfulImage[] | any[]
-    slidesPerView?: number
-    slug: string
-    isProperties?: boolean
-    spaceBetween?: number
-    fixedHeight?: number
+  items: ContentfulImage[] | any[];
+  slidesPerView?: number | 'auto';
+  slug: string;
+  isProperties?: boolean;
+  centeredSlides?: boolean;
+  spaceBetween?: number;
+  fixedHeight?: number;
+  slidesOffsetBefore?: number;
+  slidesOffsetAfter?: number;
 }
 
-class ImageGridSlider extends Component {
-    private readonly images: any
-    private readonly slidesPer: number | string
-    private readonly slug: string
-    private readonly isProperties: boolean
-    private readonly spaceBetween: number
-
-    constructor(props: SwiperProps) {
-        super(props)
-        const {
-            items,
-            slidesPerView,
-            slug,
-            isProperties = false,
-            spaceBetween = 0,
-            fixedHeight,
-        } = props as SwiperProps
-
-        this.isProperties = isProperties
-        this.images = items
-        this.slug = slug
-        this.slidesPer = slidesPerView || 'auto'
-        this.spaceBetween = spaceBetween
-
-        this.state = {
-            swiper: undefined,
-            height: fixedHeight,
-        }
-    }
-
-    componentDidUpdate(
-        prevProps: Readonly<{ slug: string; fixedHeight: number }>,
-        prevState: Readonly<{ swiper: Swiper }>,
-        snapshot?: any
-    ) {
-        if (prevProps.fixedHeight !== this.props.fixedHeight) {
-            this.setState({
-                height: this.props.fixedHeight,
-            })
-        }
-
-        if (prevProps.slug !== this.slug) {
-            prevState.swiper.slideTo(0)
-        }
-    }
-
-    nextSlide() {
-        if (this.state.swiper) {
-            const { activeIndex, isBeginning, isEnd } = this.state.swiper
-            // @ts-ignore
-            const { items } = this.props
-            const nextIndex =
-                activeIndex + 1 <= items.length ? activeIndex + 1 : activeIndex
-            this.state.swiper.slideTo(nextIndex)
-        }
-    }
-
-    prevSlide() {
-        if (this.state.swiper) {
-            const { activeIndex, isBeginning, isEnd } = this.state.swiper
-            // @ts-ignore
-            const { items } = this.props
-            const nextIndex = activeIndex - 1 < 0 ? 0 : activeIndex - 1
-            this.state.swiper.slideTo(nextIndex)
-        }
-    }
-
-    render() {
-        return (
-            <>
-              {  this.images.length > 1 && <NavigationWrap className="navigation-wrap">
-                    <div onClick={() => this.prevSlide()}>
-                        <svg viewBox="0 0 8 16">
-                            <polyline points="0 0, 8 8, 0 16" />
-                        </svg>
-                    </div>
-                    <div onClick={() => this.nextSlide()}>
-                        <svg viewBox="0 0 8 16">
-                            <polyline points="0 0, 8 8, 0 16" />
-                        </svg>
-                    </div>
-                </NavigationWrap>}
-                <StyledSwiper
-                    loop={false}
-                    spaceBetween={this.spaceBetween}
-                    // freeMode
-                    grabCursor={true}
-                    slidesPerView={this.slidesPer}
-                    onSwiper={(swiper) => this.setState({ swiper: swiper })}
-                    onUpdate={(swiper) => {
-                        swiper.slideTo(0)
-                    }}
-                >
-                    {this.images &&
-                        this.images.length &&
-                        this.images.map((x: any, i: number) => {
-                            return (
-                                <>
-                                    <SwiperSlide
-                                        className="next"
-                                        key={`${this.slug}-${
-                                            Math.random() * i
-                                        }`}
-                                    >
-                                        {this.isProperties ? (
-                                            <PropertyGridItem collapsed propertyObj={x} />
-                                        ) : (
-                                            <GridImage
-                                                fixedHeight={this.state.height}
-                                                sizes="50vw"
-                                                ratio="natural"
-                                                imageObj={x}
-                                                border={false}
-                                                borderRadius={true}
-                                            />
-                                        )}
-                                    </SwiperSlide>
-                                </>
-                            )
-                        })}
-                </StyledSwiper>
-            </>
-        )
-    }
+interface SwiperState {
+  swiper?: Swiper;
+  height?: number;
 }
 
-export default ImageGridSlider
+class ImageGridSlider extends Component<SwiperProps, SwiperState> {
+  private readonly images: any[];
+  private readonly slug: string;
+  private readonly isProperties: boolean;
+  private readonly centeredSlides: boolean;
+  private readonly spaceBetween: number;
+  private readonly slidesOffsetBefore: number;
+  private readonly slidesOffsetAfter: number;
+
+  constructor(props: SwiperProps) {
+    super(props);
+
+    const {
+      items,
+      slug,
+      centeredSlides = false,
+      slidesOffsetBefore = 10,
+      slidesOffsetAfter = 10,
+      isProperties = false,
+      spaceBetween = 0,
+      fixedHeight,
+    } = props;
+
+    this.images = items || [];
+    this.slug = slug;
+    this.isProperties = isProperties;
+    this.centeredSlides = centeredSlides;
+    this.spaceBetween = spaceBetween;
+    this.slidesOffsetBefore = slidesOffsetBefore;
+    this.slidesOffsetAfter = slidesOffsetAfter;
+
+    this.state = {
+      swiper: undefined,
+      height: fixedHeight,
+    };
+  }
+
+  componentDidUpdate(prevProps: SwiperProps) {
+    if (prevProps.fixedHeight !== this.props.fixedHeight) {
+      this.setState({ height: this.props.fixedHeight });
+    }
+
+    if (prevProps.slug !== this.slug && this.state.swiper) {
+      this.state.swiper.slideTo(0);
+    }
+  }
+
+  nextSlide = () => {
+    const { swiper } = this.state;
+    if (swiper) {
+      const nextIndex = Math.min(swiper.activeIndex + 1, this.images.length - 1);
+      swiper.slideTo(nextIndex);
+    }
+  };
+
+  prevSlide = () => {
+    const { swiper } = this.state;
+    if (swiper) {
+      const prevIndex = Math.max(swiper.activeIndex - 1, 0);
+      swiper.slideTo(prevIndex);
+    }
+  };
+
+  render() {
+    const { height } = this.state;
+
+    // Define responsive breakpoints for slidesPerView and spaceBetween
+    const breakpoints = {
+      320: {
+        spaceBetween: 15,
+      },
+      768: {
+        spaceBetween: 15,
+      },
+      1024: {
+        spaceBetween: 20,
+      },
+    };
+
+    return (
+      <>
+        {this.images.length > 1 && (
+          <NavigationWrap className="navigation-wrap">
+            <div onClick={this.prevSlide}>
+              <svg viewBox="0 0 8 16">
+                <polyline points="0 0, 8 8, 0 16" />
+              </svg>
+            </div>
+            <div onClick={this.nextSlide}>
+              <svg viewBox="0 0 8 16">
+                <polyline points="0 0, 8 8, 0 16" />
+              </svg>
+            </div>
+          </NavigationWrap>
+        )}
+        <StyledSwiper
+          loop={false}
+          spaceBetween={this.spaceBetween}
+        //   slidesOffsetBefore={this.slidesOffsetBefore}
+        //   slidesOffsetAfter={this.slidesOffsetAfter}
+          grabCursor={true}
+          slidesPerView={'auto'}
+          centeredSlides={this.centeredSlides}
+          breakpoints={breakpoints} // Pass breakpoints to Swiper
+          onSwiper={(swiper) => this.setState({ swiper })}
+        >
+          {this.images.map((x, i) => (
+            <SwiperSlide key={`${this.slug}-${i}`}>
+              {this.isProperties ? (
+                <PropertyGridItem collapsed propertyObj={x} />
+              ) : (
+                <GridImage
+                  sizes="100vw"
+                  fixedHeight={height}
+                  ratio="natural"
+                  imageObj={x}
+                  border={false}
+                  borderRadius={true}
+                />
+              )}
+            </SwiperSlide>
+          ))}
+        </StyledSwiper>
+      </>
+    );
+  }
+}
+
+export default ImageGridSlider;
