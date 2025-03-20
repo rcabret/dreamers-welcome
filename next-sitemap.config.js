a023562a799cc884e01f6a5d43829806d482d8f2
akshita-tomar <akshita.tomar@ultivic.com>
2025-03-20 18:12:48 +0530

    exclude selected urls from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,44 @@ const client = require('contentful').createClient({
   host: 'cdn.contentful.com'
 })
 
+const excludedUrls = [
+  "https://eluxemagazine.com/travel/10-fully-vegan-hotels-around-the-world",
+  "https://inmagazine.ca/2023/11/welcome-to-san-juan",
+  "https://www.revistalternativa.com/noticias-estilo-de-vida/asi-son-los-lugares-mas-originales-para-alojarse-en-puerto-rico-39676",
+  "https://www.greenmatters.com/community/green-city-guide-san-juan-puerto-rico",
+  "https://www.luxurylifestylemag.co.uk/travel/5-top-luxury-hotels-and-resorts-in-puerto-rico-to-book-right-now/?fbclid=PAAabfEvoIFw3lQYb7N3PJWhx62YJvuPB5oJDCwMDRq_n4_xreNv7RVpPYIQw",
+  "https://www.insider.com/where-to-stay-in-san-juan-puerto-rico-best-hotels#dreamcatcher-by-dw-6",
+  "https://www.insider.com/guides/travel/best-hotels-cheap-luxury-bathrooms#the-dreamcatcher-1",
+  "https://www.insider.com/guides/travel/best-hotels-puerto-rico#the-dreamcatcher-1",
+  "https://hotelsabovepar.com/the-best-boutique-hotels-in-north-carolina",
+  "https://www.brit.co/best-places-to-visit",
+  "https://hotelsabovepar.com/the-coolest-affordable-boutique-hotels-in-puerto-rico-2",
+  "https://www.axios.com/local/raleigh/2022/08/19/guide-weekend-visit-wilmington-nc",
+  "https://www.lonelyplanet.com/articles/best-weekend-getaways-for-us-couples",
+  "https://www.cbsnews.com/essentials/great-new-hotels-to-book-in-2022-in-the-united-states",
+  "https://www.architecturaldigest.com/story/the-7-most-satisfying-renovation-instagram-accounts-to-follow-right-now",
+  "https://www.culturedmag.com/article/2022/01/25/a-self-made-spirit-and-a-hefty-dose-of-creativity-have-made-san-juan-puerto-rico-a-must-see-art-destination",
+  "https://www.domino.com/content/dreamers-welcome-wilmington",
+  "https://vegnews.com/vegan-travel/vegan-hotels-in-america",
+  "https://pursuitist.com/4-reasons-to-visit-eco-friendly-puerto-rico",
+  "https://www.lonelyplanet.com/articles/best-lgbtq-spots-in-san-juan",
+  "https://www.thetravel.com/budget-friendly-hotels-in-puerto-rico",
+  "https://drifttravel.com/city-guide-san-juan-puerto-rico",
+  "https://www.travelandleisure.com/weekend-getaways-in-the-south-7098671",
+  "https://togo.hotelbusiness.com/article/dreamers-welcome-offers-inclusivity-and-authenticity",
+  "https://www.cntraveler.com/gallery/best-airbnbs-in-puerto-rico",
+  "https://www.dezeen.com/2023/12/30/verde-guesthouse-dreamers-welcome-san-juan-puerto-rico",
+  "https://hotelsabovepar.com/the-best-affordable-boutique-hotels-in-the-caribbean",
+  "https://upgradedpoints.com/travel/hotels/best-boutique-hotels-in-puerto-rico",
+  "https://www.trazeetravel.com/10-budget-friendly-high-design-airbnbs-in-san-juan-puerto-rico",
+  "https://vegnews.com/vegan-travel/vegan-hotels-in-america",
+  "https://www.vogue.com/article/an-insiders-guide-to-san-juan-puerto-rico",
+  "https://pursuitist.com/5-places-to-experience-first-class-wellness-in-puerto-rico",
+  "https://passportmagazine.com/explore-lgbtq-san-juan-puerto-rico",
+  "https://www.theglobeandmail.com/life/travel/article-i-wanted-more-than-a-boring-beach-vacation-so-i-took-my-family-to",
+  "https://www.townandcountrymag.com/leisure/travel-guide/a63225670/san-juan-puerto-rico-travel-guide"
+];
+
 
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://dreamerswelcome.com',
@@ -18,7 +56,8 @@ module.exports = {
     });
   
     
-    const slugs = entries.items.map(item => {
+    const slugs = entries.items
+    .map(item => {
         if (item.fields && item.fields.slug && item.fields.slug.startsWith('https')) { 
           return item.fields.slug;
         } else if (item.fields && item.fields.slug) {   
@@ -26,7 +65,8 @@ module.exports = {
         } else {   
           return `${config.siteUrl}/news/field?Id=${item.sys.id}`;
         }
-    });
+    })
+    .filter(slug => !excludedUrls.includes(slug));
      
     return slugs;
     };
