0e058510eea4dbd799f4e8dff229a32ad0297961
Shivam Gaind <shivam.ultivic@gmail.com>
2025-07-03 19:31:18 +0530

    redeploy

--- src/_lib/api.ts
+++ src/_lib/api.ts
@@ -31,6 +31,7 @@ export const getLandingpage = async () => {
 }
 
 
+
 export const getHomepage = async (url: string) => {
     const entries = await client.getEntries({
         content_type: 'homepage',
