7572e03b6c39ef669eb64c44d393698d7ed55ec4
shivam-ultivic <43299805+shivam-ultivic@users.noreply.github.com>
2025-07-29 23:04:45 +0530

    Update next.config.js
    next config change

--- next.config.js
+++ next.config.js
@@ -20,58 +20,62 @@ const nextConfig = {
     },
 
     // Add redirects here
-    async redirects() {
-        return [
-             {
-                source: '/:path*',
-                has: [{ type: 'host', value: 'www.dreamerswelcome.com' }],
-                has: [{ type: 'host', value: 'dreamerswelcome.com' }],
-                destination: 'https://dreamerswelcome.com/:path*',
-                permanent: true,
-            },
-            {
-                source: '/drift-away', // The path you want to redirect from
-                destination: '/', // The path you want to redirect to
-                permanent: true, // Set to true for permanent redirect (301)
-            },
-            {
-                source: '/experiences',
-                destination: '/experiences/puertorico',
-                permanent: true,
-            },
-            {
-                source: '/guidebooks',
-                destination: '/guidebooks/puertorico',
-                permanent: true,
-            },
-            {
-                source: '/northcarolina',
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/experiences/northcarolina',
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/guidebooks/northcarolina',
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/puertorico',
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/dreamers-nc',
-                destination: '/',
-                permanent: true,
-            },
+async redirects() {
+  return [
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'www.dreamerswelcome.com',
+        },
+      ],
+      destination: 'https://dreamerswelcome.com/:path*',
+      permanent: true,
+    },
+    {
+      source: '/drift-away',
+      destination: '/',
+      permanent: true,
+    },
+    {
+      source: '/experiences',
+      destination: '/experiences/puertorico',
+      permanent: true,
+    },
+    {
+      source: '/guidebooks',
+      destination: '/guidebooks/puertorico',
+      permanent: true,
+    },
+    {
+      source: '/northcarolina',
+      destination: '/',
+      permanent: true,
+    },
+    {
+      source: '/experiences/northcarolina',
+      destination: '/',
+      permanent: true,
+    },
+    {
+      source: '/guidebooks/northcarolina',
+      destination: '/',
+      permanent: true,
+    },
+    {
+      source: '/puertorico',
+      destination: '/',
+      permanent: true,
+    },
+    {
+      source: '/dreamers-nc',
+      destination: '/',
+      permanent: true,
+    },
+  ];
+}
 
-        ]
-    }
 }
 
 module.exports = nextConfig
